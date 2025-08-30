6e88b891afdeee3062ff80fa74cc3d0a774516d8
Mohd Adil <shaikhmdadil191@gmail.com>
2025-08-30 16:08:29 +0530

    Update sketch_mar20a.ino

--- sketch_mar20a.ino
+++ sketch_mar20a.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid = "Shravanya";
-const char* password = "shravanya<3";
+const char* ssid = "";
+const char* password = "";
 
 ESP8266WebServer server(80);
 
